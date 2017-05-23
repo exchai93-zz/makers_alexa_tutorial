270dfdd6a7024273ce016722ef4112f181441ead
Emily Chai <echai93@gmail.com>
2017-05-23 14:48:08 +0100

    Refactor so that response is built from the initializer to JSON

--- lib/alexa/response.rb
+++ lib/alexa/response.rb
@@ -1,17 +1,18 @@
 require 'json'
 
 module Alexa
-  class Response
+  class Response < Hash
+
+    def initialize
+      self[:version] = "1.0"
+      self[:response] = Hash.new
+      self[:response][:outputSpeech] = Hash.new
+      self[:response][:outputSpeech][:type] = "PlainText"
+      self[:response][:outputSpeech][:text] = "Hello World"
+    end
+
     def self.build
-      {
-        version: "1.0",
-        response: {
-          outputSpeech: {
-            type: "PlainText",
-            text: "Hello World"
-          }
-        }
-      }.to_json
+      new.to_json
     end
   end
 end
