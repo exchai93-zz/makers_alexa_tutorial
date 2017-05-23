03a950148284005512628d351543d3b83bb25c4d
Emily Chai <echai93@gmail.com>
2017-05-23 17:12:40 +0100

    Refactor server.rb to use new NumberFact object

--- server.rb
+++ server.rb
@@ -2,31 +2,17 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 require 'lib/alexa/response'
+require 'lib/number_fact'
 
 post '/' do
   alexa_request = Alexa::Request.new(request)
-
-  number_facts_uri = URI("http://numbersapi.com/#{
-  alexa_request.slot_value("Number") }/#{
-  alexa_request.slot_value("FactType") }")
-  number_fact = Net::HTTP.get(number_facts_uri)
-
-  Alexa::Response.build(number_fact)
-
+  number_fact = NumberFact.new(alexa_request.slot_value("Number"),
+  alexa_request.slot_value("FactType"))
+    Alexa::Response.build(number_fact.text)
 end
+
 
 def fetch_number_fact(number, fact_type)
   number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
   number_fact = Net::HTTP.get(number_facts_uri)
-end 
-# post '/' do
-#   {
-#    version: "1.0",
-#    response: {
-#      outputSpeech: {
-#        type: "PlainText",
-#        text: "Hello World"
-#      }
-#    }
-#  }.to_json
-# end
+end
