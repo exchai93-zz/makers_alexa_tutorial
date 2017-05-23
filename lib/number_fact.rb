2831152f9c1ef69b8acf8094b76342cee1b31fc2
Emily Chai <echai93@gmail.com>
2017-05-23 17:18:52 +0100

    Further refactor to extract slot value

--- lib/number_fact.rb
+++ lib/number_fact.rb
@@ -7,4 +7,10 @@ class NumberFact
     number_fact_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
     @text = client.get(number_fact_uri)
   end
+
+  def self.build(alexa_request, client = Net::HTTP)
+    new(alexa_request.slot_value("Number"),
+    alexa_request.slot_value("FactTyle"))
+  end
+  
 end
--- server.rb
+++ server.rb
@@ -5,9 +5,7 @@ require 'lib/alexa/response'
 require 'lib/number_fact'
 
 post '/' do
-  alexa_request = Alexa::Request.new(request)
-  number_fact = NumberFact.new(alexa_request.slot_value("Number"),
-  alexa_request.slot_value("FactType"))
+  NumberFact.build(Alexa::Request.new(request))
     Alexa::Response.build(number_fact.text)
 end
 
