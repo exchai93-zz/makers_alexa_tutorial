656f6e733bf31695530de1c33b28d9fccf53a1aa
Emily Chai <echai93@gmail.com>
2017-05-23 15:47:14 +0100

    Exchange hard coded 'Hello World' for the variable response_text

--- lib/alexa/response.rb
+++ lib/alexa/response.rb
@@ -3,16 +3,36 @@ require 'json'
 module Alexa
   class Response < Hash
 
-    def initialize
+    def initialize(response_text)
       self[:version] = "1.0"
       self[:response] = Hash.new
       self[:response][:outputSpeech] = Hash.new
       self[:response][:outputSpeech][:type] = "PlainText"
-      self[:response][:outputSpeech][:text] = "Hello World"
+      self[:response][:outputSpeech][:text] = response_text
     end
 
-    def self.build
-      new.to_json
+    def self.build(response_text = "Hello World")
+      new(response_text).to_json
     end
   end
 end
+
+#
+# post '/' do
+#   parsed_request = JSON.parse(request.body.read)
+#   number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
+#   fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
+#
+#   number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
+#   number_fact = Net::HTTP.get(number_facts_uri)
+#
+#   {
+#     version: "1.0",
+#     response: {
+#       outputSpeech: {
+#           type: "PlainText",
+#           text: number_fact
+#         }
+#     }
+#   }.to_json
+# end
