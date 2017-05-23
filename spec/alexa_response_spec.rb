a0aa4fb11041f251aafaf9ec4653c940afe48493
Emily Chai <echai93@gmail.com>
2017-05-23 14:21:26 +0100

    Tests JSON to be returned whenever Alexa::Response.build is called

---
+++ spec/alexa_response_spec.rb
@@ -0,0 +1,20 @@
+require 'alexa/response'
+
+RSpec.describe Alexa::REsponse do
+  describe './build' do
+    it 'returns a minimal JSON response' do
+      minimal_response = {
+        version: "1.0",
+        response: {
+          outputSpeech: {
+            type: "PlainText",
+            text: "Hello World"
+          }
+        }
+      }.to_json
+
+      expect(Alexa::Response.build).to eq
+minimal_response
+    end
+  end
+end
